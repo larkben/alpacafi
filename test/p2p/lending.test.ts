ae95b42916e4800560847d3d1395171a78e81d76
Benjamin <60051088+larkben@users.noreply.github.com>
2025-02-25 14:28:19 +0000

    unit tests prototypes

--- test/p2p/lending.test.ts
+++ test/p2p/lending.test.ts
@@ -7,7 +7,7 @@ import {
 } from "@alephium/web3";
 import { PrivateKeyWallet } from "@alephium/web3-wallet";
 import { getSigners, testAddress } from "@alephium/web3-test";
-import { alph, randomP2PKHAddress } from "../create-token/utils";
+import { randomP2PKHAddress, alph } from "./helperFunctions";
   
 const nodeProvider = new NodeProvider("http://127.0.0.1:22973");
   
@@ -26,7 +26,7 @@ describe("lending p2p coverage + tests", () => {
       //collectionTemplate = (await deployCollection()).contractInstance
     }, 100000);
   
-    test('', async () => {
+    test('create loan, edit collateral, cancel loan', async () => {
       const creator = buyer[0]
       const spender = buyer[1]
 
@@ -34,4 +34,60 @@ describe("lending p2p coverage + tests", () => {
     
         
     })
+
+    test('create loan, accept loan, repay loan, check fees', async () => {
+      const creator = buyer[0]
+      const spender = buyer[1]
+
+      
+    
+        
+    })
+
+    test('create loan, accept loan, forfeit, auction, bid redeem', async () => {
+      const creator = buyer[0]
+      const spender = buyer[1]
+
+      
+    
+        
+    })
+
+    test('create loan, accept loan, forfeit, auction, instant redeem', async () => {
+      const creator = buyer[0]
+      const spender = buyer[1]
+
+      
+    
+        
+    })
+
+    test('create loan, accept loan, (attempt) liquidation(s)', async () => {
+      const creator = buyer[0]
+      const spender = buyer[1]
+
+      
+    
+        
+    })
+
+    test('create loan, accept loan, liquidation, auction creation, bid, and redeem, check fees', async () => {
+      const creator = buyer[0]
+      const spender = buyer[1]
+
+      
+    
+        
+    })
+
+    test('create loan, accept loan, liquidation, auction creation, instant redeem, check fees', async () => {
+      const creator = buyer[0]
+      const spender = buyer[1]
+
+      
+    
+        
+    })
+
+    //? no liquidation loans
 });
