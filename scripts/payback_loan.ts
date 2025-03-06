125af1705f56a4719dfb3c7e730cb4c957882871
Benjamin Larkey <spartinofarrows@gmail.com>
2025-03-05 22:15:34 -0600

    usdt finalized

--- scripts/payback_loan.ts
+++ scripts/payback_loan.ts
@@ -11,7 +11,7 @@ const dotenv = require('dotenv');
 dotenv.config()
 
 const nodeProvider = new NodeProvider('https://node.mainnet.alephium.org')                  // Mainnet
-const signer = new PrivateKeyWallet({ privateKey: String(process.env.key), nodeProvider })
+const signer = new PrivateKeyWallet({ privateKey: String(process.env.twokey), nodeProvider })
 
 const deployScript: DeployFunction<Settings> = async (
     deployer: Deployer,
@@ -22,10 +22,10 @@ const deployScript: DeployFunction<Settings> = async (
     let tx = await PayLoan.execute(signer, {
       initialFields: {
           loanFactory: "e1cc79ca2a9c3143426ffc3fbab5ac14550cb7c4ef94793598101b31cf831900",
-          contract: "86725cc49c4851103bce427bb71a414feca07a0955ac3e4d6dad10ea95337000"
+          contract: "dda0c05f078b24d6c5822eef876dfa057eb0df16e077b30f0c2203de790fe000"
       },
       attoAlphAmount: DUST_AMOUNT + (MINIMAL_CONTRACT_DEPOSIT * 2n),
-      tokens: [{id: "556d9582463fe44fbd108aedc9f409f69086dc78d994b88ea6c9e65f8bf98e00", amount: 55420000n}]
+      tokens: [{id: "556d9582463fe44fbd108aedc9f409f69086dc78d994b88ea6c9e65f8bf98e00", amount: 10000000n}]
     })
 
     // should add this everywhere
